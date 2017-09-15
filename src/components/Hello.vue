222464c4f7730fc457aaa742900e4f3fd45b9f64
Wei <840325394@qq.com>
2017-09-15 18:12:11 +0800

    vueapp

---
+++ src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+  	<router-view></router-view>
+    <mu-paper style="width: 100%; position: fixed;bottom: 0;">
+    <mu-bottom-nav :value="bottomNav" shift @change="handleChange">
+      <mu-bottom-nav-item to='test' value="movies" title="Movies" icon="ondemand_video"/>
+      <mu-bottom-nav-item value="music" title="Music" icon="music_note"/>
+      <mu-bottom-nav-item value="books" title="Books" icon="books"/>
+      <mu-bottom-nav-item value="pictures" title="Pictures" icon="photo"/>
+    </mu-bottom-nav>
+  </mu-paper>
+  </div>
+  
+</template>
+
+<script>
+	import test from './test.vue'
+export default {
+  name: 'hello',
+ data () {
+    return {
+      bottomNav: 'movies',
+      bottomNavColor: 'movies'
+    }
+  },
+  methods: {
+    handleChange (val) {
+      this.bottomNav = val
+    }
+  }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		1023456798156
+	</div>
+	
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
